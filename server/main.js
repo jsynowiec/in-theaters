9d7c938e43c375ab3ad191e2c754c56d82c4e53d
Jakub Synowiec <jakub.synowiec@gmail.com>
2016-07-12 21:47:13 +0200

    Add API with /movies endpoint, pass tests

---
+++ server/main.js
@@ -0,0 +1,64 @@
+const Hapi = require('hapi');
+const Joi = require('joi');
+const good = require('good');
+const Boom = require('boom');
+
+const getMovieTimes = require('./helpers/movie-times');
+const errors = require('./messages/errors');
+
+const server = new Hapi.Server();
+
+server.connection({
+  host: 'localhost',
+  port: process.env.PORT || 8080,
+});
+
+server.route({
+  method: 'GET',
+  path: '/movies',
+  handler: (request, reply) => {
+    getMovieTimes(request.query.title, request.query.city)
+      .then((result) => (result.length > 0 ? result : Boom.notFound(errors.NO_THEATERS_AVAILABLE)))
+      .then((result) => reply(result));
+  },
+  config: {
+    validate: {
+      query: {
+        title: Joi.string().required(),
+        city: Joi.string().required(),
+      },
+    },
+  },
+});
+
+const options = {
+  ops: {
+    interval: 1000,
+  },
+  reporters: {
+    console: [{
+      module: 'good-squeeze',
+      name: 'Squeeze',
+      args: [{ log: '*', response: '*' }],
+    }, {
+      module: 'good-console',
+    }, 'stdout'],
+  },
+};
+
+server.register({
+  register: good,
+  options,
+}, (err) => {
+  if (err) {
+    server.log(['error'], err);
+
+    throw err;
+  }
+
+  server.start(() => {
+    server.log(['debug'], `Server started at ${server.info.uri}`);
+  });
+});
+
+module.exports = server;
---
+++ server/messages/errors.js
@@ -0,0 +1,3 @@
+module.exports = {
+  NO_THEATERS_AVAILABLE: 'Movie not found or not in theaters around provided city.',
+};
