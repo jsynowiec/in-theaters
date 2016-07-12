51165a3f40a8faf168361a5a73903e852285a6a0
Jakub Synowiec <jakub.synowiec@gmail.com>
2016-07-12 20:06:52 +0200

    Fix movie showtimes scraper, pass tests
     - remove illegal zero-width characters,
     - filter showtimes,

--- server/helpers/movie-times.js
+++ server/helpers/movie-times.js
@@ -1,6 +1,8 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
 
+const illegalCharacters = /\u200B|\u200C|\u200D|\uFEFF|\u200E|\u200F/gi;
+
 module.exports = function getMovieTimes(title, city, language = 'en') {
   return request({
     uri: 'https://www.google.com/movies',
@@ -16,11 +18,14 @@ module.exports = function getMovieTimes(title, city, language = 'en') {
     // Scrape movie times and theater names
     $('.theater').each((i, el) => {
       const theater = {
-        name: $('.name', el).text(),
-        address: $('.address', el).text(),
+        name: $('.name', el).text().replace(illegalCharacters, '')
+          .trim(),
+        address: $('.address', el).text().replace(illegalCharacters, '')
+          .trim(),
         times: $('.times', el).text()
           .split(' ')
-          .map((time) => time.trim()),
+          .filter((time) => /(\d{1,2}:\d{1,2})(pm)?/ig.test(time))
+          .map((time) => time.replace(illegalCharacters, '').trim()),
       };
 
       if (theater.times.length > 0) {
